c4ce87da0a25b6115acd6b15aacc3f854cc319f0
grey300 <tgyel3002@gmail.com>
2026-08-03 16:45:08 +0600

    fix(revenue): register Settlement + Market entities for reconcile DI

--- src/markets/revenue-distribution.module.ts
+++ src/markets/revenue-distribution.module.ts
@@ -2,13 +2,16 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { RevenueDistribution } from "../entities/revenue-distribution.entity";
+import { Settlement } from "../entities/settlement.entity";
+import { Market } from "../entities/market.entity";
 import { RevenueDistributionService } from "./revenue-distribution.service";
 import { PaymentModule } from "../payment/payment.module";
 
 @Module({
   imports: [
     ConfigModule,
-    TypeOrmModule.forFeature([RevenueDistribution]),
+    // Settlement + Market are needed by reconcileMissingDistributions().
+    TypeOrmModule.forFeature([RevenueDistribution, Settlement, Market]),
     PaymentModule,
   ],
   providers: [RevenueDistributionService],
